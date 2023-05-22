bb466978e3cad3d64b1a55849669cfefa887acef
Mooonkins <Quasarjae72@gmail.com>
2023-05-22 12:48:43 +0900

    23/05/22

---
+++ Uni_Run/Assets/01.Scripts/Platform.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    private bool stepped = false;
+
+    private void OnEnable()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
