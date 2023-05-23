588cd5e2778ecfc1b4ff074b85caed1093afbd71
Mooonkins <Quasarjae72@gmail.com>
2023-05-23 12:45:54 +0900

    23/05/23

--- Uni_Run/Assets/01.Scripts/Platform.cs
+++ Uni_Run/Assets/01.Scripts/Platform.cs
@@ -9,10 +9,26 @@ public class Platform : MonoBehaviour
 
     private void OnEnable()
     {
-        
+        stepped = false;
+
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            if (Random.Range(0, 3) == 0)            
+                obstacles[i].SetActive(true);
+            else
+                obstacles[i].SetActive(false);
+        }
+        /*foreach (GameObject i in obstacles)
+        {
+            i.SetActive(Random.Range(0, 3) == 0 ? true : false);
+        }*/
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.CompareTag("Player") && !stepped)
+        {
+            stepped = true;
+            GameManager.instance.AddScore(10);
+        }
     }
 }
